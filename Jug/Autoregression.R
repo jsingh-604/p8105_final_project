6caa2b1b017c31a924ac6bf92d2517efaacbb03f
Jagjit Singh <jag@jagjits-mbp.dyn.nyp.org>
2022-11-29 17:17:30 -0500

    Adding shiny app and testing application

--- Jug/Autoregression.R
+++ Jug/Autoregression.R
@@ -6,6 +6,7 @@ ny_covid = read.csv('data/covid_testing.csv')  %>%
 ###############################
 ny_covid$test_date = lubridate::mdy(ny_covid$test_date)
 sub_df = ny_covid[ny_covid$county == 'New York City', ]
+plot(sub_df$new_positives)
 counties = levels(as.factor(ny_covid$county))
 county_df_list = list()
 # loop version 2
@@ -18,11 +19,24 @@ net_df = matrix(nrow = 900)
 for (i in county_df_list){
   net_df = cbind(net_df,i$new_positives[1:900])
 }
+net_df = cbind(net_df,i$test_date[1:900])
 net_df = net_df[,-c(1)]
 net_df = data.frame(net_df)
 colnames(net_df) = c(counties)
 
-########  ACF plot #######
+corr <- cor(net_df)
+library(ggplot2)
+library(ggcorrplot)
+ggcorrplot(corr, sig.level=0.05, lab_size = 4.5, p.mat = NULL,
+          insig = c("pch", "blank"), pch = 1, pch.col = "black", pch.cex =1,
+          tl.cex = 14) +
+  # Order: top, right, bottom, left
+  theme(axis.text.x = element_text(margin=margin(-2,0,0,0), size = 5),
+        axis.text.y = element_text(margin=margin(0,-2,0,0), size = 5))
+
+
+
+################## ACF plot ############
 library(forecast)
 conf.level <- 0.95
 ciline <- qnorm((1 - conf.level)/2)/sqrt(length(sub_df$new_positives))
@@ -34,7 +48,8 @@ q <- ggplot(data=bacfdf, mapping=aes(x=lag, y=acf)) +
   geom_bar(stat = "identity", position = "identity",fill="lightblue")  
 q
 
-### Auto regression ####
-reg1 = Arima(sub_df$new_positives, order = c(10,0,0))
+############## Auto regression ############
+reg1 = auto.arima(net_df$`New York City`)
 summary(reg1)
-
+plot(net_df$`New York City`,col="red")
+lines(fitted(reg1),col="blue")
